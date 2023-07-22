2afffaa5d9397ab0cd750459be5f0ff978418879
hydraxl <adam.freed123@gmail.com>
2023-07-22 03:57:45 -0700

    forgot to add updated main file to last commit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import poorly_designed_but_working_gui as gui
+import gui
 from point import *
 from bezier_curve import *
+
+graph = gui.GUI()
 
 P1 = Point(0, 0)
 P2 = Point(0, 1)
@@ -10,9 +12,5 @@ control_points = [P1, P2, P3, P4]
 
 spline = Bezier(control_points)
 
-samples = spline.sample(30)
-
-gui.plot_lines(samples)
-gui.plot_points(control_points)
-
-gui.display()
+graph.add_spline(spline, samples=30)
+graph.display()
