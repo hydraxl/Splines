4be1fc01bf246328f482b4a80987e644759b3005
hydraxl <adam.freed123@gmail.com>
2023-07-22 13:08:59 -0700

    fixed syntax errors

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-import gui
+from gui import *
 from point import *
 from bezier_curve import *
 
-graph = gui.GUI()
+import numpy as np
+
+graph = GUI()
 
 P1 = Point(0, 0)
 P2 = Point(0, 1)
 P3 = Point(1, 1)
 P4 = Point(1, 0)
-control_points = [P1, P2, P3, P4]
 
+control_points = np.array([P1, P2, P3, P4])
 spline = Bezier(control_points)
 
 graph.add_spline(spline, samples=30)
