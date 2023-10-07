09629cd01967e613529b17b861af49aa51fd3f3c
hydraxl <adam.freed123@gmail.com>
2023-10-07 02:17:55 -0700

    Added Bezier Splines

---
+++ bezier_spline.py
@@ -0,0 +1,52 @@
+from spline import Spline
+import numpy as np
+
+class Bezier_Spline(Spline):
+
+    # Sample n points in each section of the spline
+    def sample(self, n=10):
+        # Added for readability
+        num_controls = len(self.control_points)
+
+        # Each section should contain exactly 4 control points, one of which is shared with the previous section
+        if (num_controls - 1) % 3 != 0:
+            raise ValueError(f"Bezier Spline has {len(self.control_points)} control points, which is not one more than a multiple of 3")
+        
+        # Setup variables
+        num_sections = (num_controls - 1) / 3
+        line_length = (n * num_sections) + 1
+        line = np.empty((line_length, 2))
+        
+        # Sample the spline
+        for s in range(num_sections):
+            section = self.control_points[s*3 : s*3 + 4]
+            
+            # Use linear interpolation to sample each bezier curve within the spline
+            for i in range(n):
+
+                # Create initial set of temporary points
+                temp_points = np.empty((3, 2))
+                for p in range(3):
+                    new_point = (section[p] * (n - i) + section[p + 1] * i) / n
+                    temp_points[p] = new_point
+
+                # Create new temporary points from old temporary points until only 1 point remains
+                while len(temp_points) > 1:
+                    new_points = np.empty((len(temp_points) - 1, 2))
+                    for p in range(len(temp_points) - 1):
+                        new_point = (temp_points[p] * (n - i) + temp_points[p + 1] * i) / n
+                        new_points[p] = new_point
+                    temp_points = new_points
+                
+                # Add remaining point to the line
+                line[i + s*n] = temp_points[0]
+            
+        # Final point on the line will always be the final control point
+        line[len(line) - 1] = np.copy(self.control_points[num_controls - 1])
+        
+        return line
+
+
+
+
+
