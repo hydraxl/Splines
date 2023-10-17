54a0c4d9cfc1942ecb6e1b658e4976a2a87051a5
hydraxl <adam.freed123@gmail.com>
2023-10-16 17:21:36 -0700

    Error handling for splines with improper numbers of control points

--- bezier_spline.py
+++ bezier_spline.py
@@ -3,30 +3,33 @@ import numpy as np
 
 class Bezier_Spline(Spline):
 
+    # Number of control points needed to add a new section of curve
+    section_points = 3
+
     # Sample n points in each section of the spline
     def sample(self, n=10):
         # Added for readability
         num_controls = len(self.control_points)
 
         # Each section should contain exactly 4 control points, one of which is shared with the previous section
-        if (num_controls - 1) % 3 != 0:
-            raise ValueError(f"Bezier Spline has {len(self.control_points)} control points, which is not one more than a multiple of 3")
+        if (num_controls - 1) % self.section_points != 0:
+            raise ValueError(f"Bezier Spline has {len(self.control_points)} control points, which is not one more than a multiple of {self.section_points}")
         
         # Setup variables
-        num_sections = (num_controls - 1) / 3
+        num_sections = int((num_controls - 1) / self.section_points)
         line_length = (n * num_sections) + 1
         line = np.empty((line_length, 2))
         
         # Sample the spline
         for s in range(num_sections):
-            section = self.control_points[s*3 : s*3 + 4]
+            section = self.control_points[s*self.section_points : (s+1)*self.section_points + 1]
             
             # Use linear interpolation to sample each bezier curve within the spline
             for i in range(n):
 
                 # Create initial set of temporary points
-                temp_points = np.empty((3, 2))
-                for p in range(3):
+                temp_points = np.empty((self.section_points, 2))
+                for p in range(self.section_points):
                     new_point = (section[p] * (n - i) + section[p + 1] * i) / n
                     temp_points[p] = new_point
 
@@ -43,7 +46,7 @@ class Bezier_Spline(Spline):
             
         # Final point on the line will always be the final control point
         line[len(line) - 1] = np.copy(self.control_points[num_controls - 1])
-        
+
         return line
 
 
--- spline.py
+++ spline.py
@@ -4,6 +4,9 @@ import numpy as np
 # generic spline class only ever used as a parent class
 # contains standard info and functions that every spline class must contain
 class Spline:
+
+    section_points = 1
+
     def __init__(self, control_points):
         self.control_points = control_points
 
@@ -13,8 +16,12 @@ class Spline:
     
     # adds new control points before the current set
     def add_front(self, new_points):
+        if len(new_points) % self.section_points != 0:
+            raise ValueError(f"Additions to this curve must have a multiple of {self.section_points} control points")
         self.control_points = new_points + self.control_points
     
     # adds new control points after the current set
     def add_back(self, new_points):
+        if len(new_points) % self.section_points != 0:
+            raise ValueError(f"Additions to this curve must have a multiple of {self.section_points} control points")
         self.control_points += new_points
